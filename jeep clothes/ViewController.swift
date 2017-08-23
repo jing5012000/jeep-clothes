399b67dfe608df67eb8d141970ee841c139ce9b2
Lucy <lucy@LucydeMacBook-Air.local>
2017-08-24 01:12:44 +0800

    Initial Commit

---
+++ jeep clothes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jeep clothes
+//
+//  Created by Lucy on 2017/8/24.
+//  Copyright © 2017年 Lucy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
